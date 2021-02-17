38ef61667b09363950884f578b95c976d43805b4
igorwigo <igor.v.q@hotmail.com>
2021-02-17 14:29:49 -0300

    implementação da mascara

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import Clipboard from 'v-clipboard'
-Vue.use(Clipboard)
+import VueMask from 'v-mask'
+Vue.use(VueMask);
+Vue.use(Clipboard);
 Vue.config.productionTip = false
 
 new Vue({
